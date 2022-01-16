1b52967fd7655b1f97063ca6d5043b7b5f226cdd
dfaua <danyil.fedorov@gmail.com>
2022-01-16 23:01:42 +0200

    semi result

--- lab6/task_1.py
+++ lab6/task_1.py
@@ -10,6 +10,9 @@ from salsa20 import XSalsa20_xor
 database = []
 database_homecity_phonenumber = []
 
+temp_key = b'"\xee\x93k\xe1C\x96\xa4\xb2\x8a\xe6\x03\xe4\xc9\xd9\xec\xb2zb\x18\xfaO\x11\xc1\x14\x1e\t\xaa\x07v-\xa6'
+temp_nonce = b'\x17\xbb\xf2\x8d}\xad\xa7\x81J4f\x03\x0c\xca\xd2g9\x8f\x15"OR\xd0\x15'
+
 
 def hashing_password(password_to_hash):
     salt = uuid.uuid4().hex
@@ -31,12 +34,13 @@ def ciphering_something(text_to_cipher):
     return list_cipher_key_nonce
 
 def ciphering_something_key_nonce(list_tocipher_key_nonce):
-    cipher_text = XSalsa20_xor(list_tocipher_key_nonce[0].encode(), list_tocipher_key_nonce[1].encode(), list_tocipher_key_nonce[2].encode())
+    print("ciphering_something_key_nonce income: ", list_tocipher_key_nonce)
+    cipher_text = XSalsa20_xor(list_tocipher_key_nonce[0].encode(), list_tocipher_key_nonce[2], list_tocipher_key_nonce[1])
     return cipher_text
 
-def deciphering_something(list_to_decipher):
-    print("deciphering something income: ", list_to_decipher)
-    deciphered = XSalsa20_xor(list_to_decipher[1], list_to_decipher[3], list_to_decipher[2])
+def deciphering_something(list_toDecipher_key_nonce):
+    print("deciphering something income: ", list_toDecipher_key_nonce)
+    deciphered = XSalsa20_xor(list_toDecipher_key_nonce[1], list_toDecipher_key_nonce[3], list_toDecipher_key_nonce[2])
     return deciphered
 
 
@@ -51,23 +55,24 @@ def registration(list_login_password_homecity_phonenumber):
     list_login_hash_key_nonce = [user_login, list_cipher_key_nonce[0], list_cipher_key_nonce[1], list_cipher_key_nonce[2]]
     database.append(list_login_hash_key_nonce)
 
-    key = "\xbf)J\xe1Lv\x1amS\x86\xa2\xff*\xce\xfc\x99k0\xc2w>\xd7\x1c\xf0\xa6\xa4\xc3\x96m\x84\xf3I".encode()
-    nonce = "b\x8f'm7\x14<\x00l=;\xd0,\x13d\x81e\x92|\xddic\xb1\t".encode()
-
     str_homecity_phonenumber = home_city + ":" + phone_number
-    ciphered_homecity_phonenumber = ciphering_something_key_nonce([str_homecity_phonenumber, key, nonce])
+    ciphered_homecity_phonenumber = ciphering_something_key_nonce([str_homecity_phonenumber, temp_key, temp_nonce])
     to_db_user_homecity_phonenumber = [user_login, ciphered_homecity_phonenumber]
+    database_homecity_phonenumber.append(to_db_user_homecity_phonenumber)
 
     for i in database:
-        print(i)
+        print("user, pass", i)
+    for i in database_homecity_phonenumber:
+        print("hc pn: ", i)
     return "Done"
 
-def check_user(list_login_password):
+def check_user(list_login_password_userToCheck):
     user = []
-    print(list_login_password)
+    user_to_check = list_login_password_userToCheck[2]
+    print(list_login_password_userToCheck)
     for i in database:
         print("I'm in 0001")
-        if i[0] == list_login_password[0]:
+        if i[0] == list_login_password_userToCheck[0]:
             print("in 0002")
             print(i)
             user = i
@@ -79,12 +84,24 @@ def check_user(list_login_password):
     #print("check user -- str to list: ", deciphered_hash_salt)
     #list_hash_salt = deciphered_hash_salt.split(":")
     list_hash_salt = list(deciphered_hash_salt.split(":"))
-    list_to_hashing = [list_login_password[1], list_hash_salt[1].encode()]
+    list_to_hashing = [list_login_password_userToCheck[1], list_hash_salt[1].encode()]
     hashed_new_pas = hashing_password_salt(list_to_hashing)
     if hashed_new_pas == list_hash_salt[0]:
-        return "User found"
+        login_answer = "user found"
+        user_to_check_answer = ""
+        user_to_check_from_db = []
+        for i in database_homecity_phonenumber:
+            if i[0] == user_to_check:
+                user_to_check_from_db = i
+                str_to_dechiper = user_to_check_from_db[1]
+                str_hm_pn_userToCheck = deciphering_something([str_to_dechiper, temp_key, temp_nonce])
+                list_plain_homecity_phonenumber = str_hm_pn_userToCheck.decode().split(":")
+                user_to_check_answer = "User: " + user_to_check + "Home city: " + list_plain_homecity_phonenumber[0] + "Phone number: " + list_plain_homecity_phonenumber[1]
+            if not user_to_check_from_db:
+                user_to_check_answer = "no such user in db"
+        return [login_answer, user_to_check_answer]
     else:
-        return "Password is incorrect"
+        return ["Password is incorrect", ""]
 
 
 

19bef840b3f8a593b2d45399d5f112a22f83df0d
dfaua <danyil.fedorov@gmail.com>
2022-01-16 13:52:05 +0200

    Добавляю сальса20 для хэша + ключа первого шифрования

--- lab5/task 1.py
+++ lab5/task 1.py
@@ -1,4 +1,19 @@
 import hashlib
+import uuid
+from simpl
 
-hash_object = hashlib.md5(b'Hello World')
-print(hash_object.hexdigest())
+
+hash_object = hashlib.sha512(b'password')
+hex_dig = hash_object.hexdigest()
+
+
+def hashing_password(password_to_hash):
+    salt = uuid.uuid4().hex
+    hashed_password = hashlib.sha512(salt.encode() + password_to_hash.encode()).hexdigest()
+    list_password_salt = [hashed_password, salt]
+    return list_password_salt
+
+
+
+
+print(hashing_password("12345"))
